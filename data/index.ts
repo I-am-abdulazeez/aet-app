3cd7c8a6fbcefba6ac70fd49791eb6287165e33e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-16 10:18:25 +0100

    feat: resuable data across application

---
+++ data/index.ts
@@ -0,0 +1,161 @@
+import type { CardContentType, HomeBgContentType, NavLinksType } from '~/types';
+
+export const NavLinks: NavLinksType[] = [
+  {
+    text: 'Home',
+    path: '/',
+  },
+  {
+    text: 'About',
+    path: '/about',
+  },
+  {
+    text: 'Products & Services',
+    path: '/product',
+  },
+  {
+    text: 'Industry',
+    path: '/industry',
+  },
+  {
+    text: 'Career',
+    path: '/career',
+  },
+  {
+    text: 'Gallery',
+    path: '/gallery',
+  },
+
+  {
+    text: 'Contact',
+    path: '/contact',
+  },
+];
+
+export const ProductCardContent: CardContentType[] = [
+  {
+    cardTitle: ' Electrical Contracting Service',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FpowerPlant.jpg?alt=media&token=cf0504d6-e755-4739-b974-d49cca3fbefe',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-1.jpg?alt=media&token=64fa3bae-1d6b-4fb1-b126-09677f93699a',
+    cardText:
+      'ANTRUM ENGINEERING is an experienced and reputable electrical contractor which provides professional electrical construction, installation testing and commissioning. We deliver project on time and within budget…..',
+
+    cardRoute: '/product',
+  },
+  {
+    cardTitle: 'Power Plant Development & Installations',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2F3d.jpg?alt=media&token=c2617a76-94a4-4fca-9772-6490cc38969e',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2F3d-blur.jpg?alt=media&token=419afc0f-4c49-4b3d-93ed-a0f22298a9b5',
+    cardText:
+      'Every application (Industrial or Residential) has its peculiar power demand (requirement) which informs the requisite power solution to be implemented….',
+
+    cardRoute: '/product',
+  },
+  {
+    cardTitle: `Power Infrastructure Management`,
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FpowerInfra.jpg?alt=media&token=28786c86-dc40-4a58-b9bb-eeee0169ddda',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-2.jpg?alt=media&token=2d37f080-79ee-4f88-b11c-a3f829ad38e4',
+    cardText: `Proper power management is germane for reliable power and desirable equipment/ infrastructure useful life span. Adherence to the OEM’s (Original Equipment Manufacturer) operational and maintenance standards; use of requisite tools and relevant technology ensures a reliable system.
+`,
+
+    cardRoute: '/product',
+  },
+  {
+    cardTitle: 'Electrical Panels',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fshowcase%2Fshow-10.jpg?alt=media&token=8baa552a-7fb3-4d64-9c71-b62d6118d20b',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-15.jpg?alt=media&token=64e751d7-9f56-4993-a5c8-517fc6c4b913',
+    cardText:
+      'For proper electrical gadget installation and standardisation, Electrical panel form an integral part of the installation from where trained personnel can monitor and control the functioning of…',
+
+    cardRoute: '/product',
+  },
+  {
+    cardTitle:
+      'Metering Service Provision – Specification, Supply & Installation',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fsmart-meter%20(1).jpg?alt=media&token=20a33733-2dd9-49b5-b222-d0601c0b41c6',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-16.jpg?alt=media&token=3da95fb8-0f65-4a34-ae94-44f1c8c894c9',
+    cardText:
+      'Being accustomed to the Nigeria metering code, standards and specification in relation to the customer’s needs, AET has delivered successfully in metering service provision ranging from……',
+
+    cardRoute: '/product',
+  },
+  {
+    cardTitle: 'Solar Inverter System',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FsolarPanels.jpg?alt=media&token=fcc8a7ba-902e-46e7-b7e0-39bb1649bc82',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-4.jpg?alt=media&token=929f9bd7-8508-43f0-9a0b-ceace7ae1c1e',
+    cardText: `AET has several experiences in solar electrification and inverter for industries, hospital, pharmacy others where uninterruptible power is paramount.
+      We offer installation, maintenance and supply of PV solar panel, batteries, inverters, charge…..
+    `,
+
+    cardRoute: '/product',
+  },
+  {
+    cardTitle: `Generator Maintenance and Escalation intervention`,
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FCAT-Big-Generator-Prices-in-Nigeria-nigeriantech.com_.ng_.jpg?alt=media&token=d939e65c-9a4d-4355-b79c-ed04abb96603',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-17.jpg?alt=media&token=71516845-0eb3-43be-a5e3-fdc24da6391f',
+    cardText:
+      'Good maintenance culture is key for an efficient diesel gen set unit. Rather than just filters and oil change, generator maintenance entails regular checks and part replacement at earmarked hours of operations as recommended by the OEM….',
+
+    cardRoute: '/product',
+  },
+  {
+    cardTitle: 'Rental power',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Frental-power.jpg?alt=media&token=10ee5e2a-31f3-41d9-adf0-c3b990d10165',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-9.jpg?alt=media&token=c205f58e-8b4a-4e56-a071-d6a44822bfd3',
+    cardText: `Emergency stop gap is inevitable for most application without redundancy of times of unplanned breakdowns, short term projects, such as construction / development works, upgrade works, others.
+    Such requires dependable rugged generator to satisfy immediate needs with no hassles…
+    `,
+
+    cardRoute: '/product',
+  },
+];
+
+export const HomeBgContent: HomeBgContentType[] = [
+  {
+    src: 'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/carousel%2Fbg-1.jpg?alt=media&token=54c5e28a-af29-4ad5-a726-13a9e253bfcc',
+    title: 'DELIVERING PROFICIENT TECHNICAL / AUTOMATED POWER SOLUTIONS ',
+    subtitle: `We offer various forms of Power engineering services that encompass Power project development, Implementation, Standardization and Commissioning.
+Our scope covers Grid, off grid, Island and Independent Power involving Planning, Installation, Testing and Commissioning.
+`,
+  },
+  {
+    src: 'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/carousel%2Fbg-3.jpg?alt=media&token=2bb7d2c9-f5ae-40b2-a13b-c42d6c255a9f',
+    title: 'REAL TIME DATA ORIENTED AND EFFICIENT POWER MANAGEMENT ',
+    subtitle:
+      '24/7 on-site operation and maintenance support with backend remote assistance banking on real time data for instant technical advice that help to ensure immediate resolution of escalation for dependable power.',
+  },
+  {
+    src: 'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/carousel%2Fbg-2.jpg?alt=media&token=bf865a48-081d-48f3-b739-3ff76fa1efce',
+    title:
+      'INNOVATIVE RECOMMENDATION AND COST EFFECTIVE CLIENT- CENTRIC APPROACH ',
+    subtitle:
+      'AET has over a decade years of experience in PIM (power infrastructure management); Rural, Industrial and Residential electrification; High, Medium and Low Voltage Projects; Solar- Inverter Alternative Power; and Accustomed Technical Know-how on wide range of generators SDMO, CAT, Perkins and others, medium and large sizes.',
+  },
+  {
+    src: 'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/carousel%2Fbg-4-1.jpg?alt=media&token=5a447178-ded6-4625-a318-4731b8a40572',
+    title: 'REQUIREMENT BASED CONSTRUCTION PROJECTS',
+    subtitle:
+      'We have proven experience of designing and constructing need specific solution for Electrical panels; Automation; Clear fuel supply Lines and Backup storages; HT and LT circuits, Street lighting and others.',
+  },
+];
+
+export const ViewerImages: string[] = [
+  'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fshowcase%2Fshow-3.jpg?alt=media&token=f44a8678-1f79-4f99-a09d-9db0166d5bf5',
+  'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fshowcase%2Fshow-8.jpg?alt=media&token=400f964d-2acb-460d-9edc-b2df7917c04e',
+];
