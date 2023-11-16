907c02b1a2c5c1de534e6a4487ca90d7681e233d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-16 10:18:51 +0100

    types: application Types and props

---
+++ types/index.ts
@@ -0,0 +1,18 @@
+export type NavLinksType = {
+  text: string;
+  path: string;
+};
+
+export type CardContentType = {
+  cardTitle: string;
+  cardImage: string;
+  cardImageLazyLoad: string;
+  cardText: string;
+  cardRoute: string;
+};
+
+export type HomeBgContentType = {
+  src: string;
+  title: string;
+  subtitle: string;
+};
