a6bcf8e9a50c1d5a85467a94cdd7414edc649fc2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-16 10:19:22 +0100

    chore: struture Layout in Navbar and Footer compos

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <VApp fixed app :elevation="0">
+      <Navbar />
+      <VMain>
+        <slot />
+      </VMain>
+      <!-- <Footer /> -->
+      <VFooter fixed app class="px-10">
+        <div class="mx-auto">
+          <img src="@/assets/power.jpeg" class="brand pt-2" alt="AET logo" />
+        </div>
+      </VFooter>
+    </VApp>
+  </nav>
+</template>
