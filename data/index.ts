43cb02bd5302a8556c1791e5d1aedbaa5ce06226
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-24 18:29:30 +0100

    feat: added industry data. refactor: remove cardRoute from CardContentType because it is not neccessary since they all route to /product

--- data/index.ts
+++ data/index.ts
@@ -41,8 +41,6 @@ export const ProductCardContent: CardContentType[] = [
       'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-1.jpg?alt=media&token=64fa3bae-1d6b-4fb1-b126-09677f93699a',
     cardText:
       'ANTRUM ENGINEERING is an experienced and reputable electrical contractor which provides professional electrical construction, installation testing and commissioning. We deliver project on time and within budget…..',
-
-    cardRoute: '/product',
   },
   {
     cardTitle: 'Power Plant Development & Installations',
@@ -52,8 +50,6 @@ export const ProductCardContent: CardContentType[] = [
       'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2F3d-blur.jpg?alt=media&token=419afc0f-4c49-4b3d-93ed-a0f22298a9b5',
     cardText:
       'Every application (Industrial or Residential) has its peculiar power demand (requirement) which informs the requisite power solution to be implemented….',
-
-    cardRoute: '/product',
   },
   {
     cardTitle: `Power Infrastructure Management`,
@@ -63,8 +59,6 @@ export const ProductCardContent: CardContentType[] = [
       'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-2.jpg?alt=media&token=2d37f080-79ee-4f88-b11c-a3f829ad38e4',
     cardText: `Proper power management is germane for reliable power and desirable equipment/ infrastructure useful life span. Adherence to the OEM’s (Original Equipment Manufacturer) operational and maintenance standards; use of requisite tools and relevant technology ensures a reliable system.
 `,
-
-    cardRoute: '/product',
   },
   {
     cardTitle: 'Electrical Panels',
@@ -74,8 +68,6 @@ export const ProductCardContent: CardContentType[] = [
       'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-15.jpg?alt=media&token=64e751d7-9f56-4993-a5c8-517fc6c4b913',
     cardText:
       'For proper electrical gadget installation and standardisation, Electrical panel form an integral part of the installation from where trained personnel can monitor and control the functioning of…',
-
-    cardRoute: '/product',
   },
   {
     cardTitle:
@@ -86,8 +78,6 @@ export const ProductCardContent: CardContentType[] = [
       'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-16.jpg?alt=media&token=3da95fb8-0f65-4a34-ae94-44f1c8c894c9',
     cardText:
       'Being accustomed to the Nigeria metering code, standards and specification in relation to the customer’s needs, AET has delivered successfully in metering service provision ranging from……',
-
-    cardRoute: '/product',
   },
   {
     cardTitle: 'Solar Inverter System',
@@ -98,8 +88,6 @@ export const ProductCardContent: CardContentType[] = [
     cardText: `AET has several experiences in solar electrification and inverter for industries, hospital, pharmacy others where uninterruptible power is paramount.
       We offer installation, maintenance and supply of PV solar panel, batteries, inverters, charge…..
     `,
-
-    cardRoute: '/product',
   },
   {
     cardTitle: `Generator Maintenance and Escalation intervention`,
@@ -109,8 +97,6 @@ export const ProductCardContent: CardContentType[] = [
       'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fblur-17.jpg?alt=media&token=71516845-0eb3-43be-a5e3-fdc24da6391f',
     cardText:
       'Good maintenance culture is key for an efficient diesel gen set unit. Rather than just filters and oil change, generator maintenance entails regular checks and part replacement at earmarked hours of operations as recommended by the OEM….',
-
-    cardRoute: '/product',
   },
   {
     cardTitle: 'Rental power',
@@ -121,8 +107,6 @@ export const ProductCardContent: CardContentType[] = [
     cardText: `Emergency stop gap is inevitable for most application without redundancy of times of unplanned breakdowns, short term projects, such as construction / development works, upgrade works, others.
     Such requires dependable rugged generator to satisfy immediate needs with no hassles…
     `,
-
-    cardRoute: '/product',
   },
 ];
 
@@ -158,4 +142,63 @@ Our scope covers Grid, off grid, Island and Independent Power involving Planning
 export const ViewerImages: string[] = [
   'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fshowcase%2Fshow-3.jpg?alt=media&token=f44a8678-1f79-4f99-a09d-9db0166d5bf5',
   'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fshowcase%2Fshow-8.jpg?alt=media&token=400f964d-2acb-460d-9edc-b2df7917c04e',
+];
+
+export const IndustriesCardContent: CardContentType[] = [
+  {
+    cardTitle: 'Real Estate',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-2.jpg?alt=media&token=a9f163f1-beee-4d75-a26d-ba41885d0ed1',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-2.jpg?alt=media&token=57ed2eba-0364-4103-a28e-04c9dea83393',
+  },
+  {
+    cardTitle: 'Commercial, Industrial & residential building',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-7.jpg?alt=media&token=645c2f33-f6b3-4ee1-9a57-89872b028285',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-7.jpg?alt=media&token=ee96644f-8a0b-4494-89f4-0bb1514f66e0',
+  },
+  {
+    cardTitle: 'Telecom',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-5.jpg?alt=media&token=dbac563f-f876-4b2a-992c-9a3bcb2e90f5',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-5.jpg?alt=media&token=e1ded4bc-9b6b-42b0-ae0b-4637a58d5661',
+  },
+  {
+    cardTitle: 'Manufacturing and Pharmaceuticals',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-4.jpg?alt=media&token=17e103c2-ab59-46ca-aad7-e283f62d6d3c',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-4.jpg?alt=media&token=20ae48b4-0d80-4598-8377-0e12396b78ef',
+  },
+  {
+    cardTitle: 'Hospital',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-1.jpg?alt=media&token=8934c880-6d39-4cf5-9b6d-166ece9c9bb3',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-1.jpg?alt=media&token=688578e6-311a-4c76-8284-ba618cc596f9',
+  },
+  {
+    cardTitle: 'Media House',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-6.jpg?alt=media&token=9fa2240d-ac10-4bf6-b7b0-3b4723d8d101',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-6.jpg?alt=media&token=8135a053-98ec-431f-9ff0-db1e2c7e18a4',
+  },
+  {
+    cardTitle: 'Agriculture',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-8.jpg?alt=media&token=bb27e320-62bc-42b2-8c72-edf06d338c06',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-8.jpg?alt=media&token=09207f17-fba2-4122-80f5-aa7f6f622686',
+  },
+  {
+    cardTitle: 'Food and Beverages',
+    cardImage:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-3.jpg?alt=media&token=67ba7034-f300-40be-9ca9-df0df3c59c4b',
+    cardImageLazyLoad:
+      'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Findustries%2Findus-blur-3.jpg?alt=media&token=a96cbd73-ca16-4ecc-987c-8c7e9d3cdf98',
+  },
 ];
