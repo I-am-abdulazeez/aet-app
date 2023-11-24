e2b48709799d2ee2bb4796d6198e43deae44ad76
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-24 06:52:57 +0100

    feat: Divider component

---
+++ components/CustomDivider/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="divide"></div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.divide {
+  width: 3em;
+  height: 0.2em;
+  margin: 0 auto;
+  background-color: rgba(255, 129, 40, 0.253);
+}
+</style>
