cd93b67012889b5b849b5cbac57f28280d83f738
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-24 18:27:19 +0100

    feat: Industry component

---
+++ components/Industry/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="indus">
+    <h1 class="text-center headline font-weight-medium">
+      Industry
+      <CustomDivider />
+    </h1>
+    <VRow align="center" justify="center" class="mt-5">
+      <VCol
+        cols="12"
+        lg="4"
+        md="4"
+        v-for="(industry, index) in IndustriesCardContent"
+        :key="index"
+        sm="12"
+      >
+        <VCard>
+          <VImg
+            height="250"
+            :src="industry.cardImage"
+            :lazy-src="industry.cardImageLazyLoad"
+            cover
+          ></VImg>
+          <VCardItem>
+            <VCardSubtitle class="subtitle-1 font-weight-medium">
+              {{ industry.cardTitle }}
+            </VCardSubtitle>
+          </VCardItem>
+        </VCard>
+      </VCol>
+    </VRow>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IndustriesCardContent } from '~/data';
+</script>
