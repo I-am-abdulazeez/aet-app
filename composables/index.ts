84b44626f3d7e103c9a8408df1ee9841f6367779
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-27 11:04:46 +0100

    feat: write beautiful composables for name and Email Validation on form

--- composables/index.ts
+++ composables/index.ts
@@ -3,3 +3,30 @@ export const toTitleCase = (str: string) => {
     return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
   });
 };
+
+export const useNameValidator = () => {
+  const validateName = (value: string) => {
+    return !value
+      ? 'Name is required'
+      : value.length > 20
+      ? 'Name must be greater than 20 characters.'
+      : true;
+  };
+
+  const nameValidationRule = (value: string) => [validateName(value)];
+
+  return { nameValidationRule };
+};
+
+export const useEmailValidator = () => {
+  const validateEmail = (value: string) =>
+    !value
+      ? 'Email is required.'
+      : !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+      ? 'Invalid email address.'
+      : true;
+
+  const emailValidationRule = (value: string) => [validateEmail(value)];
+
+  return { emailValidationRule };
+};
