6a2c93600fade70cbff0da8fb08f83e197467788
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-27 11:05:11 +0100

    chore: home page done

--- pages/index.vue
+++ pages/index.vue
@@ -5,99 +5,59 @@
       <Meta name="description" content="About Nuxi Store" />
     </Head>
     <HomeCarousel :items="HomeBgContent" />
+    <VContainer>
+      <h3 class="text-h5 text-center my-5 animated bounce">
+        Product and Services
+        <CustomDivider />
+      </h3>
+      <CardCarousel :items="ProductCardContent" />
+    </VContainer>
     <VContainer class="mt-5">
-      <VRow>
-        <VCol cols="12" lg="6" md="6" sm="12">
-          <div>
-            <h1 class="text-center headline mb-2 font-weight-medium">
-              About us
-              <div class="divide"></div>
-            </h1>
-            <div>
-              <h3 class="text-subtitle-1 my-5 animated slideInRight">
-                <span class="font-weight-regular">
-                  ANTRUM ENGINEERING AND TECHNOLOGY COMPANY (NIGERIA) LIMITED
-                  (AET) RC1246959</span
-                >
-                is a wholly Nigerian engineering company incorporated under the
-                laws of the Federal Republic of Nigeria to harness the capacity
-                to render Electrical Power Engineering, Management and
-                Consultancy Services having been successful with executing
-                several electrical projects with vast experience garnered since
-                2005 from its conception and registration as AET’ Company.
-              </h3>
-              <VBtn variant="text" color="secondary" nuxt to="/about"
-                >Read More</VBtn
-              >
-            </div>
-          </div>
-        </VCol>
-        <VCol cols="12" lg="6" md="6" sm="12">
-          <div>
-            <VRow>
-              <VCol cols="12" lg="12">
-                <h1 class="text-center headline font-weight-medium">
-                  Showcase
-                  <div class="divide"></div>
-                </h1>
-                <div class="mt-2 text-center">
-                  <h4 class="text-center font-weight-medium mb-5 grey--text">
-                    Our work speaks more about us...
-                  </h4>
-                  <viewer :images="ViewerImages">
-                    <img
-                      class="image"
-                      v-for="(src, index) in ViewerImages"
-                      :src="src"
-                      :key="index"
-                      :alt="`${src}-${index}`"
-                    />
-                  </viewer>
-                </div>
-              </VCol>
-              <!-- <VBtn
-                color="secondary"
-                variant="tonal"
-                nuxt
-                to="/gallery"
-                size="small"
-                class="mt-5"
-                >View More</VBtn
-              > -->
-            </VRow>
-          </div>
-        </VCol>
-      </VRow>
-      <VContainer>
-        <VRow class="my-5">
-          <VCol cols="12" lg="12" sm="12">
-            <h3 class="text-h5 text-center animated bounce">
-              Product and Services
-              <div class="divide"></div>
-            </h3>
-          </VCol>
-        </VRow>
-        <CardCarousel :items="ProductCardContent" />
-      </VContainer>
+      <div>
+        <h1 class="text-center headline mb-2 font-weight-medium">
+          About us
+          <CustomDivider />
+        </h1>
+        <div>
+          <h3 class="text-subtitle-1 my-5 animated slideInRight">
+            <span class="font-weight-regular">
+              ANTRUM ENGINEERING AND TECHNOLOGY COMPANY (NIGERIA) LIMITED (AET)
+              RC1246959
+            </span>
+            is a wholly Nigerian engineering company incorporated under the laws
+            of the Federal Republic of Nigeria to harness the capacity to render
+            Electrical Power Engineering, Management and Consultancy Services
+            having been successful with executing several electrical projects
+            with vast experience garnered since 2005 from its conception and
+            registration as AET’ Company.
+          </h3>
+          <VBtn
+            class="text-capitalize"
+            color="secondary"
+            size="small"
+            flat
+            nuxt
+            to="/about"
+          >
+            About us
+          </VBtn>
+        </div>
+      </div>
     </VContainer>
 
-    <div>
+    <div class="bg-secondary">
       <VContainer>
-        <!-- <indus /> -->
+        <h3 class="text-subtitle-1 text-center">
+          We see teamwork on every project as a key success element. The
+          over-all project manager on each project knows to ensure efficient and
+          regular communication to all stakeholders and at all levels.
+        </h3>
       </VContainer>
     </div>
 
-    <div class="secondary">
+    <div>
       <VContainer>
-        <VRow>
-          <VCol cols="12" lg="12" md="12" sm="12">
-            <h3 class="text-subtitle-1 text-center">
-              We see teamwork on every project as a key success element. The
-              over-all project manager on each project knows to ensure efficient
-              and regular communication to all stakeholders and at all levels.
-            </h3>
-          </VCol>
-        </VRow>
+        <Industry />
       </VContainer>
     </div>
 
@@ -108,18 +68,20 @@
     <div id="contact">
       <VContainer>
         <h3 class="pa-3 text-center text-h5 font-weigt-medium">
-          Contact
-          <div class="divide"></div>
+          Reach out to us
         </h3>
-        <!-- <Contact /> -->
+        <CustomDivider />
+        <ReachUs />
       </VContainer>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { ViewerImages, ProductCardContent, HomeBgContent } from '~/data';
+import Viewer from 'v-viewer';
+
+import 'viewerjs/dist/viewer.css';
 </script>
 
 <style lang="scss">
