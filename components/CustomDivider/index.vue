a444a0ebb2d942d949f44613a562054e813198db
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-28 12:17:57 +0100

    chore: Adding Props to custom Divider component, to make it flexible

--- components/CustomDivider/index.vue
+++ components/CustomDivider/index.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="divide"></div>
+  <div class="divide" :class="{ 'mr-0': hasMr, 'ml-0': hasMl }"></div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { hasMr, hasMl } = defineProps({
+  hasMr: {
+    type: Boolean,
+    default: false,
+  },
+  hasMl: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 .divide {
