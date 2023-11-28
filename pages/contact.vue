5aded138f2383a24324a1fba90e8eb1002515094
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-28 18:21:19 +0100

    feat: contact page

---
+++ pages/contact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Head>
+      <Title>Contact | Antrum Engineering </Title>
+      <Meta name="description" content="Contact | Antrum Engineering " />
+    </Head>
+
+    <Header :Theader="headerProps" />
+
+    <VContainer>
+      <ReachUs />
+    </VContainer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HeaderType } from '~/types';
+
+const headerProps: HeaderType = {
+  heading: 'Contact Us',
+  secText: "Let's hear from you...",
+  headerImage:
+    'https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2Fpexels-narcisa-aciko-1292464.jpg?alt=media&token=1172a16e-c102-4395-9b97-ff1ffff9ff6d',
+};
+</script>
