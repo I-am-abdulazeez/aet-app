1307105fcec18515ffb41df510bfadfb20ad4cb9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-29 15:37:56 +0100

    feat: ScrollToTop component

---
+++ components/ScrollToTop/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <VLayoutItem
+    v-scroll="onScroll"
+    class="text-end pointer-events-none"
+    model-value
+    position="bottom"
+    size="88"
+  >
+    <div class="ma-4">
+      <VFabTransition>
+        <VBtn
+          v-show="model"
+          class="mt-auto pointer-events-initial"
+          color="accent"
+          variant="flat"
+          icon="mdi-chevron-up"
+          size="small"
+          @click="onClick"
+        />
+      </VFabTransition>
+    </div>
+  </VLayoutItem>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const model = ref(false);
+
+const onScroll = () => {
+  model.value = window.scrollY > 200;
+};
+
+const onClick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+</script>
+
+<style lang="scss" scoped></style>
