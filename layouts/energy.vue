968b7dcb7a73408bf767018087bdc6b33861bce9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-29 17:56:03 +0100

    feat: energy layout for AES route

---
+++ layouts/energy.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <VApp fixed app :elevation="0">
+      <VAppBar
+        height="50"
+        app
+        fixed
+        dense
+        scroll-behavior="elevate"
+        :scroll-threshold="1000"
+      >
+        <NuxtLink to="/" class="mx-auto">
+          <img
+            style="width: 150px"
+            src="@/assets/energy.png"
+            class="brand"
+            alt="AET"
+          />
+        </NuxtLink>
+      </VAppBar>
+      <VMain>
+        <slot />
+      </VMain>
+      <ScrollToTop />
+    </VApp>
+  </nav>
+</template>
