aab3507f2eea342cb6cca5f7688bef046ead9dcf
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-11-29 15:38:38 +0100

    chore: Link to AET Engery component

---
+++ components/AETEnergy/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <VLayoutItem model-value position="bottom" size="10" class="ml-5">
+    <VBtn
+      size="small"
+      variant="flat"
+      color="accent"
+      to="/antrum_energy"
+      rounded="xl"
+      class="font-weight-bold"
+      prepend-icon="mdi-arrow-right-top"
+    >
+      Antrum Energy
+    </VBtn>
+  </VLayoutItem>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const router = useRouter();
+
+const model = ref(false);
+</script>
+
+<style lang="scss" scoped></style>
