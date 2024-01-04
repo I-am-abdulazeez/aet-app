7159605270140c7834c69261698bb68116af48ee
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-01-04 14:49:24 +0100

    chore: AET and AET Sub

---
+++ components/AETPower/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <VLayoutItem position="bottom" size="10" class="ml-5">
+    <NuxtLink :to="to">
+      <VBtn
+        size="small"
+        variant="flat"
+        color="accent"
+        rounded="xl"
+        class="font-weight-bold float-power"
+        :prepend-icon="icon"
+      >
+        {{ text }}
+      </VBtn>
+    </NuxtLink>
+  </VLayoutItem>
+</template>
+
+<script setup lang="ts">
+const { to, icon, text } = defineProps({
+  to: {
+    type: String,
+    default: false,
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: false,
+    required: true,
+  },
+  text: {
+    type: String,
+    default: false,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss">
+.float-power {
+  position: fixed;
+  bottom: 60px;
+  left: 16px;
+  z-index: 1089;
+}
+</style>
---
+++ components/AETSub/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <VLayoutItem position="bottom" size="10" class="ml-5">
+    <NuxtLink :to="to">
+      <VBtn
+        size="small"
+        variant="flat"
+        color="accent"
+        rounded="xl"
+        class="font-weight-bold float"
+        :prepend-icon="icon"
+      >
+        {{ text }}
+      </VBtn>
+    </NuxtLink>
+  </VLayoutItem>
+</template>
+
+<script setup lang="ts">
+const { to, icon, text } = defineProps({
+  to: {
+    type: String,
+    default: false,
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: false,
+    required: true,
+  },
+  text: {
+    type: String,
+    default: false,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss">
+.float {
+  position: fixed;
+  bottom: 16px;
+  left: 16px;
+  z-index: 1080;
+}
+</style>
