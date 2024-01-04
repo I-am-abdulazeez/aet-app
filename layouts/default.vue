ed7f78e608da80176c9ebbdca4ab86d18db235a9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-01-04 14:49:45 +0100

    chore: AETSub and AETPower component

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,8 @@
       </VMain>
       <Footer />
       <ScrollToTop />
-      <AETEnergy />
+      <AETSub icon="mdi-arrow-right-top" to="/energy" text="Antrum Energy" />
+      <AETPower icon="mdi-arrow-right-top" to="/power" text="Antrum Power" />
       <VFooter>
         <div class="mx-auto">
           <img src="@/assets/power.jpeg" class="brand pt-2" alt="AET logo" />
