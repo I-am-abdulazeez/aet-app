1e0dfaf62d06c9701e89f5e36eb6bf4ba6773a0d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-01-22 17:31:41 +0100

    chore: power

--- pages/power.vue
+++ pages/power.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>Antrum Power</div>
+  <div class="font-weight-medium">
+    <VContainer>
+      <h5>Antrum Power</h5>
+    </VContainer>
+  </div>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'energy',
+});
+</script>
 
 <style lang="scss" scoped></style>
