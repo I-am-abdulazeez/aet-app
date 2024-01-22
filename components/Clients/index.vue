7b65564ffb44a6d4a4087051d876a83b62a14ba8
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-01-22 14:44:01 +0100

    new: clients

---
+++ components/Clients/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="clients">
+    <h3 class="text-center mt-4 mb-6 text-h5 font-weight-medium">
+      Our Beautiful Clients & Partners.
+      <CustomDivider />
+    </h3>
+    <!-- <carousel
+      :items="clientItems"
+      :margin="clientMargin"
+      :nav="nav"
+      :responsive="clientResponsive"
+      :autoplay="autoPlay"
+      class="pa-2"
+    >
+    </carousel> -->
+    <div>
+      <VRow>
+        <VCol
+          align-self="center"
+          cols="12"
+          sm="12"
+          md="3"
+          v-for="(client, index) in ClientsImage"
+        >
+          <VImg
+            :key="index"
+            :alt="client.id"
+            :src="client.image"
+            class="mx-auto"
+            height="100"
+          />
+        </VCol>
+      </VRow>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ClientsImage } from '~/data';
+</script>
