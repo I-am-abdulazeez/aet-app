0d95ad059283d1c79c305fb75b9a6cebbb336d36
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-01-24 07:44:26 +0100

    chore: AES rows

---
+++ components/AESValues/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <VRow align="center" class="mt-7" id="first">
+      <VCol cols="12" sm="12" md="5">
+        <VImg
+          src="https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FAES%20Images%2FFirefly%2013%20(1).jpg?alt=media&token=95d05e7a-cab1-4eae-8c79-0829b2504860"
+          lazy-src=""
+        ></VImg>
+      </VCol>
+      <VCol cols="12" sm="12" md="6" offset-md="1" offset-lg="1">
+        <VChip
+          color="secondary"
+          class="text-subtitle-2 font-weight-medium mb-4"
+        >
+          ENERGY EFFICIENCY MANAGEMENT SERVICES (EEMS)
+        </VChip>
+        <p class="text-justify font-weight-regular">
+          * Energy audit <br />
+          * Energy Monitoring and Management systems <br />
+          * Demand Response Program <br />
+          * Energy Efficient Appliances and Equipment <br />
+          * Behavioural Change and Training <br />
+          * Industrial Process Optimization
+        </p>
+      </VCol>
+    </VRow>
+    <!--First Row-->
+
+    <VRow align="center" class="mt-7" id="second">
+      <VCol cols="12" sm="12" md="5">
+        <VChip
+          color="secondary"
+          class="text-subtitle-2 font-weight-medium mb-4"
+        >
+          RENEWABLE ENERGY SOLUTIONS (RES)
+        </VChip>
+        <p class="text-justify font-weight-regular">
+          * Solar PV System <br />
+          * Solar Thermal System <br />
+          * Wind Turbine Projects<br />
+          * Tidal and Wave Energy System <br />
+          * Energy Storage Systems <br />
+          * Micro-grid and Off-grid Solution
+        </p>
+      </VCol>
+      <VCol cols="12" sm="12" md="6" offset-md="1" offset-lg="1">
+        <VImg
+          src="https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FAES%20Images%2FFirefly%202%20(1).jpg?alt=media&token=79376c81-2f97-4b33-8bf5-83d7b2b1e82d"
+        ></VImg>
+      </VCol>
+    </VRow>
+    <!--Second Row-->
+
+    <VRow align="center" class="mt-7" id="third">
+      <VCol cols="12" sm="12" md="6">
+        <VImg
+          src="https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FAES%20Images%2FFirefly%202%20(1).jpg?alt=media&token=79376c81-2f97-4b33-8bf5-83d7b2b1e82d"
+        ></VImg>
+      </VCol>
+      <VCol cols="12" sm="12" md="5" offset-md="1" offset-lg="1">
+        <VChip color="secondary" class="font-weight-medium mb-4">
+          SMART METERING SOLUTIONS (SMS)
+        </VChip>
+        <p class="= font-weight-regular text-justify">
+          * Consultation and System Design <br />
+          * Smart Meter Deployment <br />
+          * Data Management and Analytics <br />
+          * Vending and Tariff Management <br />
+          * Metering Infrastructure and Data Management System: MIM & MDMS
+          <br />
+          * Customer Support and Education
+        </p>
+      </VCol> </VRow
+    ><!--Third Row-->
+
+    <VRow align="center" class="mt-7" id="fourth">
+      <VCol cols="12" sm="12" md="6">
+        <VChip color="secondary" class="title font-weight-medium mb-4"
+          >BUILDING-INTEGRATED PHOTOVOLTAICS (BIPV) & SMART BUILDING TECHNOLOGY
+          (SBT)</VChip
+        >
+        <p class="font-weight-regular text-justify">
+          * Engineering and Integration <br />
+          * Electrical System Installation and Integration <br />
+          * Building Envelope Improvement <br />
+          * Monitoring and Maintenance <br />
+          * Workshop and Seminars <br />
+          * Environmental Impact Assessment (EIA)
+        </p>
+      </VCol>
+      <VCol cols="12" sm="12" md="5" offset-md="1" offset-lg="1">
+        <VImg
+          src="https://firebasestorage.googleapis.com/v0/b/aet-site-ac2f5.appspot.com/o/images%2FAES%20Images%2FFirefly%20A%20vibrant%20image%20of%20solar%20panels%20glistening%20under%20the%20sun%2C%20showcasing%20the%20company's%20commitmen%20(4).jpg?alt=media&token=14469b2b-20da-460d-831e-c8d3c38ed8e5"
+        ></VImg>
+      </VCol> </VRow
+    ><!--Fourth Row-->
+  </div>
+</template>
